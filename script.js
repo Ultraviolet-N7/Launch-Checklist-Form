0916855e723d19d31e70935d0d69e77357f76814
unknown <meieramberj@gmail.com>
2020-12-10 21:55:19 -0600

    Added form validation checking that user enters inputs for all fields, and that all fields contain a valid data type.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Write your JavaScript code here!
-
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){
+       let pilotNameInput = document.querySelector("input[name=pilotName]");
+       let copilotNameInput = document.querySelector("input[name=copilotName]");
+       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+       let cargoMassInput = document.querySelector("input[name=cargoMass]");
+       
+       if (pilotNameInput.value === "" || !isNaN(pilotNameInput.value)){
+           alert("Please enter a valid pilot name.");
+           event.preventDefault();
+       }
+       if (copilotNameInput.value === "" || !isNaN(copilotNameInput.value)){
+           alert("Please enter a valid co-pilot name.");
+           event.preventDefault();
+       }
+       if (fuelLevelInput.value === "" || isNaN(fuelLevelInput.value)){
+           alert("Please enter a valid fuel level.");
+           event.preventDefault(); 
+       }
+       if (cargoMassInput.value === "" || isNaN(cargoMassInput.value)){
+           alert("Please enter a valid cargo mass.");
+           event.preventDefault();
+       }
+       
+   });
+   console.log(typeof pilotNameInput.value); 
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
