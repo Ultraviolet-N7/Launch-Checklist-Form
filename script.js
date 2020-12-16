491baaff5be99d2ec7b55fb1f98312643187cffe
unknown <meieramberj@gmail.com>
2020-12-16 15:48:14 -0600

    fetched data with json, added to template literals

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${}</li>
+   <li>Diameter: ${}</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${}">
+*/
+
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
    let form = document.querySelector("form");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+    response.json().then(function(json){
+        let target = document.getElementById("missionTarget");
+        target.innerHTML = `<h2>Mission Destination</h2>
+        <ol>
+           <li>Name: ${json[0].name}</li>
+           <li>Diameter: ${json[0].diameter}</li>
+           <li>Star: ${json[0].star}</li>
+           <li>Distance from Earth: ${json[0].distance}</li>
+           <li>Number of Moons: ${json[0].moons}</li>
+        </ol>
+        <img src="${json[0].image}">`
+    });
+   });
    form.addEventListener("submit", function(event){
        let pilotNameInput = document.querySelector("input[name=pilotName]");
        let copilotNameInput = document.querySelector("input[name=copilotName]");
@@ -61,19 +87,5 @@ window.addEventListener("load", function(){
     });
     
 });
-// faultyItems.style.visibility = "visible";
-//         pilotStatus.innerHTML = `${pilotNameInput.value} is ready.`
-//         copilotStatus.innerHTML = `${copilotNameInput.value} is ready.`
-//         console.log("This worked.");
-//         event.preventDefault();
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
+
